3d0492f928b4dead4d7767527ed4b1228ae1580b
Luan Victor <luanvictordev@gmail.com>
2026-06-08 20:58:38 -0300

    fix(cors): aceita qualquer porta localhost em vez de portas fixas

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:8080', 'http://localhost:3001'],
+    origin: (origin, callback) => {
+      if (!origin || /^https?:\/\/localhost(:\d+)?$/.test(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   });
 
